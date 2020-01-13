be43f5fde5c3edd58c56239724cbccb5915f807f
Stephen Rudd <stephen.rudd@nanoporetech.com>
2020-01-13 16:35:03 +0100

    adding nanopore logo for tutorial branding

--- setup.py
+++ setup.py
@@ -12,9 +12,14 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/sagrudd/megrim",
     packages=setuptools.find_packages(),
+    package_data = {
+        'megrim': ['data/ONT_logo.png']
+    },
+    include_package_data=True,
     install_requires=['datetime', 'pytz', 'progressbar', 'tqdm', 'ipython', 
                       'ipywidgets', 'matplotlib', 'pandas', 'numpy',
-                      'pyranges', 'pysam', 'fontawesome', 'scipy', 'dask'],
+                      'pyranges', 'pysam', 'fontawesome', 'scipy', 
+                      'dask[complete]'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
