33ab94451c2f838d3a142db4f8eecbafb7c5eea7
Stephen Rudd <stephen.rudd@nanoporetech.com>
2020-01-13 15:55:00 +0100

    scipy is a slighly more cryptic dependecy

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=['datetime', 'pytz', 'progressbar', 'tqdm', 'ipython', 
                       'ipywidgets', 'matplotlib', 'pandas', 'numpy',
-                      'pyranges', 'pysam', 'fontawesome'],
+                      'pyranges', 'pysam', 'fontawesome', 'scipy'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
