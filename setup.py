bb4ce26d1abcb4c2d383a9e22e55340cfc4160ba
Stephen Rudd <stephen.rudd@nanoporetech.com>
2020-01-13 15:57:45 +0100

    and dask ...

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=['datetime', 'pytz', 'progressbar', 'tqdm', 'ipython', 
                       'ipywidgets', 'matplotlib', 'pandas', 'numpy',
-                      'pyranges', 'pysam', 'fontawesome', 'scipy'],
+                      'pyranges', 'pysam', 'fontawesome', 'scipy', 'dask'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
