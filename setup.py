1a269c36ac568b084703ff9c0cf8a2ed9dfb2908
Stephen Rudd <stephen.rudd@nanoporetech.com>
2020-02-20 19:28:10 +0100

    quick silliness fix

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sagrudd/megrim",
-    packages=setuptools.find_packages(),
+    packages=["megrim", "megrim.plugins"],
     package_data = {
         'megrim': ['data/ONT_logo.png', 'data/FontAwesome5Free-Solid-900.otf']
     },
